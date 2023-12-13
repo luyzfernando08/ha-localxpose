e6922c8f0c08bf6ff5d3d69c0fc1b9cc943478ef
Luiz Fernando <luyzfernando@gmail.com>
2023-12-13 10:37:57 -0300

    Added GUI feature

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 echo "Starting"
 loclx --version
 export ACCESS_TOKEN=$(bashio::config 'token')
-loclx tunnel config -f /config/config.yaml 
+export HTTP_LISTEN_ADDRESS=0.0.0.0:54537
+loclx tunnel config -f /config/config.yaml -gui
